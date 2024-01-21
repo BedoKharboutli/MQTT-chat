cbe62429ef1edcbbd2f6372eb46ba776580620be
bedo.kh <bedo.kharboutli@hotmail.com>
2024-01-21 17:23:36 +0100

    Code been updated to Encryption & Decryption

--- Labb2 - chat.py.py
+++ Labb2 - chat.py.py
@@ -2,6 +2,9 @@ import paho.mqtt.client as paho
 import random
 import threading
 import queue
+import rsa
+import base64
+from cryptography.fernet import Fernet, InvalidToken
 
 
 CLIENT_ID = f"kyh-mqtt-{random.randint(0, 1000)}"
@@ -15,7 +18,7 @@ CHAT_ROOMS = {"room1": "chat/group1", "room2": "chat/group2", "room3": "chat/gro
 
 
 class Chat:
-    def __init__(self, username, room):
+    def __init__(self, username, room, secret):
         self.username = username
         self.room = room
         self.topic = CHAT_ROOMS[room]
@@ -25,6 +28,14 @@ class Chat:
         # This variable is used to exit the thread when the
         # user exits the application
         self.running = True
+
+        # my32lengthsupersecretnooneknows1
+
+        key = base64.urlsafe_b64encode(bytes(secret, "utf-8"))
+        try:
+            self.fernet = Fernet(key)
+        except:
+            raise Exception(f"The secret-key must be 32 charecters")
 
     @staticmethod
     def on_connect(client, userdata, flags, rc):
@@ -48,12 +59,22 @@ class Chat:
         self.client.connect(BROKER, PORT)
 
     def on_message(self, client, userdata, message):
-        # TODO_2
-        msg = message.payload.decode("utf-8")
-        if msg.startswith(f"{self.username}"):
-            return
+        msg = message.payload
+
+        # Check if the message is encrypted
+        if msg.startswith(b"gAAAAA"):
+            try:
+                decrypted_msg_bytes = self.fernet.decrypt(msg)
+                decrypted_msg = decrypted_msg_bytes.decode("utf-8")
+            except InvalidToken:
+                print(msg)
         else:
-            print(f"{message.payload.decode('utf-8')}")
+            decrypted_msg = msg.decode("utf-8")
+
+            if decrypted_msg.startswith(str(self.username)):
+                return
+            else:
+                print(decrypted_msg)
 
     def init_client(self):
         # Subscribe to selected topic
@@ -79,7 +100,11 @@ class Chat:
         self.client.loop_start()
 
         # TODO_3
-        self.client.publish(self.topic, f"{self.username} , has joined the chat.")
+        # self.client.publish(self.topic, f"{self.username} , has joined the chat.")
+        encrypted_msg = self.fernet.encrypt(
+            f"{self.username} , has joined the chat.".encode()
+        ).decode("utf-8")
+        self.client.publish(self.topic, encrypted_msg)
 
     def run(self):
         self.init_client()
@@ -94,16 +119,24 @@ class Chat:
                 # Check if the user wants to exit the application
                 if msg_to_send.lower() == "quit":
                     # TODO_4
-                    self.client.publish(
+                    """self.client.publish(
                         self.topic, f"{self.username} , has left the chatroom."
-                    )
+                    )"""
+                    encrypted_msg = self.fernet.encrypt(
+                        f"{self.username} , has left the chatroom.".encode()
+                    ).decode("utf-8")
+                    self.client.publish(self.topic, encrypted_msg)
 
                     # Indicate to the input thread that it can exit
                     self.running = False
                     break
 
                 # TODO_5
-                self.client.publish(self.topic, f"{self.username} : {msg_to_send}")
+                # self.client.publish(self.topic, f"{self.username} : {msg_to_send}")
+                encrypted_msg = self.fernet.encrypt(
+                    f"{self.username} : {msg_to_send}".encode()
+                ).decode("utf-8")
+                self.client.publish(self.topic, encrypted_msg)
 
             except queue.Empty:  # We will end up here if there was no user input
                 pass  # No user input, do nothing
@@ -117,13 +150,14 @@ class Chat:
 def main():
     # Init application. Ask for username and chat room
     username = input("Enter your username: ")
+    secret = input("Enter the secret_key: ")
 
     print("Pick a room:")
     for room in CHAT_ROOMS:
         print(f"\t{room}")
     room = input("> ")
 
-    chat = Chat(username, room)
+    chat = Chat(username, room, secret)
     chat.run()
 
 
